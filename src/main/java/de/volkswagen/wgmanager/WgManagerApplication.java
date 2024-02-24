cb5f1cb6bcf516f513e3bea14418770b17af9aab
Thnnndrrrrrr <tsteidl@duck.com>
2024-02-24 18:49:27 +0100

    Initial Push

---
+++ src/main/java/de/volkswagen/wgmanager/WgManagerApplication.java
@@ -0,0 +1,13 @@
+package de.volkswagen.wgmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WgManagerApplication.class, args);
+    }
+
+}
